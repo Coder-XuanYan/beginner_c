bcb025a0d707ad2569bb02926915370d0ccc1574
轩言 <108106976+Coder-XuanYan@users.noreply.github.com>
2022-06-27 23:07:00 +0800

    Add files via upload

---
+++ text01_main主函数/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+//没参数时使用
+int main(void)
+{
+	//c99标准主函数的形态
+	return 0; //可省略，为代码完整性尽量写
+}
+
+////有参数时使用
+//int main(int argc, ，char** argv)//char*argv[]也可以
+//{
+//
+//	//最通用的命令行参数的主函数形态
+//	return 0;
+//}
+
